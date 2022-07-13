09aa1b2bb7ad979cd5ea5e642c3bdc39ed4248a1
Prof Nilson Mori <nilsonmori@yahoo.com>
2022-07-13 01:01:00 -0300

    auto install java and python-pip

--- src/EmbedMAS/bin/firstBoot.sh
+++ src/EmbedMAS/bin/firstBoot.sh
@@ -53,10 +53,13 @@ ln -s $EmbedMAS_HOME/bin/EmbedMAS-NetworkRestart /usr/bin/EmbedMAS-NetworkRestar
 
 
 # Instalando o JAVA
+apt install /opt/EmbedMAS/var/cache/apt/archives/libcups2_2.3.3op2-3+deb11u2_armhf.deb -y
 apt install /opt/EmbedMAS/var/cache/apt/archives/openjdk-8-jre_8u312-b07-1+rpi1_armhf.deb -y
 
 
 # Instalando o Python pyserial
+apt install /opt/EmbedMAS/var/cache/apt/archives/libexpat1_2.2.10-2+deb11u3_armhf.deb -y
+apt install /opt/EmbedMAS/var/cache/apt/archives/libexpat1-dev_2.2.10-2+deb11u3_armhf.deb -y
 apt install /opt/EmbedMAS/var/cache/apt/archives/python3-pip_20.3.4-4+rpt1_all.deb -y
 pip install --no-index --find-links /opt/EmbedMAS/var/cache/pip/ pyserial
 
